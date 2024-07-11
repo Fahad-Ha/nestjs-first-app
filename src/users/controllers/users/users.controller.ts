3658c6fdb3b363ce84c457fdbb547753e840e7f7
Fahad-Ha <fahhod.ahmad@gmail.com>
2024-07-12 02:18:18 +0300

    dto, route params and query

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers() {
+  getUsers(@Query('sortBy') sortBy: string) {
+    console.log(sortBy);
     return [{ username: 'Fahad', email: 'Fahad@test.com' }];
   }
 
@@ -25,5 +37,16 @@ export class UsersController {
         ],
       },
     ];
+  }
+
+  @Post()
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return userData;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id };
   }
 }
