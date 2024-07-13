f2c8e1887a727c48bd38802ab2ec758f35f35bfa
Fahad-Ha <fahhod.ahmad@gmail.com>
2024-07-13 19:12:55 +0300

    added a middleware

--- src/users/middlewares/test/test.middleware.ts
+++ src/users/middlewares/test/test.middleware.ts
@@ -1,8 +1,22 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class TestMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Test middleware');
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new HttpException('No Authorization Token', HttpStatus.FORBIDDEN);
+    }
+
+    console.log(authorization);
     next();
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { TestMiddleware } from './middlewares/test/test.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TestMiddleware).forRoutes(
+      {
+        path: 'users',
+        method: RequestMethod.GET,
+      },
+      {
+        path: 'users/:id',
+        method: RequestMethod.GET,
+      },
+    );
+  }
+}
