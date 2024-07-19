549a4de9f0e2dfc5800f92470c97b44ff68955ee
Fahad-Ha <fahhod.ahmad@gmail.com>
2024-07-20 01:22:50 +0300

    added Guard

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -6,10 +6,12 @@ import {
   ParseBoolPipe,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { AuthGuard } from 'src/users/guards/auth/auth.guard';
 import { ValidateCreateUserPipe } from 'src/users/pipes/validate-create-user/validate-create-user.pipe';
 import { UsersService } from 'src/users/services/users/users.service';
 
@@ -18,6 +20,7 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getUsers(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean) {
     console.log(sortDesc);
     return this.usersService.fetchUsers();
@@ -45,6 +48,7 @@ export class UsersController {
 
   @Post()
   @UsePipes(new ValidationPipe())
+  @UseGuards(AuthGuard)
   createUser(@Body(ValidateCreateUserPipe) userData: CreateUserDto) {
     return this.usersService.createUser(userData);
   }
--- src/users/pipes/validate-create-user/validate-create-user.pipe.ts
+++ src/users/pipes/validate-create-user/validate-create-user.pipe.ts
@@ -4,8 +4,6 @@ import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 @Injectable()
 export class ValidateCreateUserPipe implements PipeTransform {
   transform(value: CreateUserDto, metadata: ArgumentMetadata) {
-    console.log(metadata.metatype);
-
     value.name = value.name.toUpperCase();
     value.age = parseInt(value.age.toString());
     return value;
